90e8d560b99fdc8da31235e41fab7629224ffed4
Franxois <franxoois@gmail.com>
2022-09-28 20:24:55 +0200

    feat(wasm): improve wasm package.json

--- apps/sveltekit/vite.config.ts
+++ apps/sveltekit/vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), wasm()],
+	plugins: [sveltekit()],
 	build: {
 		target: 'esnext'
 	},
