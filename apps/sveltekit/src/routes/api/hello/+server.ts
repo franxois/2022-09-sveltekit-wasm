f759a69d7a04d44fe115f194f87ab641e1ba5aa4
Franxois <franxoois@gmail.com>
2023-09-30 10:16:09 +0200

    chore(dep): blindly update deps

--- apps/sveltekit/src/routes/api/hello/+server.ts
+++ apps/sveltekit/src/routes/api/hello/+server.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from './$types';
 
 /** Fallback if service worker is not installed */
 export const GET: RequestHandler = () => {
-	return new Response(String(JSON.stringify({ message: 'Hello from Sveltekit API' })));
+	return new Response(String(JSON.stringify({ message: 'Hello from Sveltekit API (+server.ts)' })));
 };
--- apps/sveltekit/src/service-worker.ts
+++ apps/sveltekit/src/service-worker.ts
@@ -1,3 +1,8 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
 import { build, files, prerendered, version } from '$service-worker';
 
 console.log('Ho hi from service worker');
@@ -8,7 +13,7 @@ self.addEventListener('fetch', (event) => {
 	if (event instanceof FetchEvent) {
 		if (event.request.url.endsWith('/api/hello')) {
 			event.respondWith(
-				new Response(JSON.stringify({ message: 'Hello from Service Worker' }), {
+				new Response(JSON.stringify({ message: 'Hello from event listener on FETCH request in service worker' }), {
 					headers: { 'content-type': 'text/json' }
 				})
 			);
